23ed916480f0228a3df6103a0d89937f5778172e
DaijiMomii <momii.daiji@vxv.co.jp>
2025-02-04 13:20:43 +0900

    update

--- my-vite-app/src/main.ts
+++ my-vite-app/src/main.ts
@@ -12,8 +12,9 @@ import
   Vector3, 
   Layer,
   // FlyCamera, 
-  ArcRotateCamera,
-  HemisphericLight, 
+  // ArcRotateCamera,
+  HemisphericLight,
+  UniversalCamera, 
   // MeshBuilder, 
   // StandardMaterial, 
   // Animation, 
@@ -28,7 +29,7 @@ from "@babylonjs/core";
 
 import '@babylonjs/loaders/glTF';
 
-// import * as GUI from "@babylonjs/gui";
+import * as GUI from "@babylonjs/gui";
 // import { AdvancedDynamicTexture, Button } from '@babylonjs/gui/2D';
 
 // GaussianSplattingのローダを有効化
@@ -210,8 +211,8 @@ const main = async () =>
   // Camera,Light 
   // scene.createDefaultCameraOrLight(true, true, true);
   // scene.createDefaultCameraOrLight(false, true, false);
+  const camera = new UniversalCamera( "camera1", new Vector3( 0, 1, 0 ), scene );
   // const camera = new ArcRotateCamera( "camera1", 0, 0, 5, new Vector3( 0, 1, 0 ), scene );
-  const camera = new ArcRotateCamera( "camera1", 0, 0, 5, new Vector3( 0, 1, 0 ), scene );
   camera.position = new Vector3( 0, 1, -3 );
   camera.attachControl( false ); 
   new HemisphericLight('light', new Vector3(0,0,0),scene);
@@ -337,7 +338,12 @@ const main = async () =>
 
 
   window.addEventListener("resize", () => engine.resize());
-  engine.runRenderLoop(() => scene.render());
+  engine.runRenderLoop( () => 
+    { 
+      scene.render();
+      // console.log( "update" );
+      OnCameraUpdate( camera );
+    });
 
 
 
@@ -345,31 +351,166 @@ const main = async () =>
 
   // ScreenUI/Button
   // advancedTextureForWindow = GUI.AdvancedDynamicTexture.CreateFullscreenUI("WindowCanvas");
-  // var advancedTextureButton = GUI.AdvancedDynamicTexture.CreateFullscreenUI("UI");
+  var advancedTextureButton = GUI.AdvancedDynamicTexture.CreateFullscreenUI("UI");
 
-  // var button = GUI.Button.CreateSimpleButton("but1", "Buttton");
-  // // button.left = "-300px"
-  // button.top = "-350px";
-  // button.paddingTop = "1px";
-  // button.width = "100px";
-  // button.height = "50px";
-  // button.color = "white";
-  // button.background = "green";
-  // button.onPointerDownObservable.add( async() => 
-  // {
-  //   console.log( "click" );
+  var button = GUI.Button.CreateSimpleButton("b", "Buttton");
+  // button.left = "-300px"
+  button.top = "-350px";
+  button.width = "100px";
+  button.height = "50px";
+  button.color = "white";
+  button.background = "green";
+  // button.horizontalAlignment = GUI.Control.HORIZONTAL_ALIGNMENT_CENTER;
+  // button.verticalAlignment = GUI.Control.VERTICAL_ALIGNMENT_TOP;
+  button.onPointerDownObservable.add( async() => 
+  {
+    console.log( "click" );
 
-  //   // var currentX = camera.position.x;
-  //   // currentX --;
-  //   // camera.position.x = currentX;
-  //   // camera.position = new Vector3( 0, 1, -5 );
-  //   // camera.target = new Vector3( 0, 1, 0 );
-  //   await cameraAnimation( "All", camera, scene );
+    // var currentX = camera.position.x;
+    // currentX --;
+    // camera.position.x = currentX;
+    // camera.position = new Vector3( 0, 1, -5 );
+    // camera.target = new Vector3( 0, 1, 0 );
+    // await cameraAnimation( "All", camera, scene );
   
     
-  // });
+  });
+//   button.onPointerUpObservable.add(function() {
+//     alert("you did it!");
+// });
 
-  // advancedTextureButton.addControl(button);
+  // advancedTextureButton.addControl( button );
+
+
+  var buttonTOP = GUI.Button.CreateSimpleButton("btnT", "↑");
+  buttonTOP.top = "-150px";
+  buttonTOP.left = "100px";
+  buttonTOP.width = "45px";
+  buttonTOP.height = "45px";
+  buttonTOP.color = "white";
+  buttonTOP.background = "green";
+  buttonTOP.horizontalAlignment = GUI.Control.HORIZONTAL_ALIGNMENT_LEFT;
+  buttonTOP.verticalAlignment = GUI.Control.VERTICAL_ALIGNMENT_BOTTOM;
+  buttonTOP.onPointerDownObservable.add( () => 
+  {
+    OnDirectionButtonPointerDown( "TOP" );
+  });
+  buttonTOP.onPointerUpObservable.add( () => 
+  {
+    OnDirectionButtonPointerUp( "TOP" );
+  });
+  var buttonBOTTOM = GUI.Button.CreateSimpleButton("btnD", "↓");
+  buttonBOTTOM.top = "-50px";
+  buttonBOTTOM.left = "100px";
+  buttonBOTTOM.width = "45px";
+  buttonBOTTOM.height = "45px";
+  buttonBOTTOM.color = "white";
+  buttonBOTTOM.background = "green";
+  buttonBOTTOM.horizontalAlignment = GUI.Control.HORIZONTAL_ALIGNMENT_LEFT;
+  buttonBOTTOM.verticalAlignment = GUI.Control.VERTICAL_ALIGNMENT_BOTTOM;
+  buttonBOTTOM.onPointerDownObservable.add( () => 
+  {
+    OnDirectionButtonPointerDown( "BOTTOM" );
+  });
+  buttonBOTTOM.onPointerUpObservable.add( () => 
+  {
+    OnDirectionButtonPointerUp( "BOTTOM" );
+  });
+  var buttonLEFT = GUI.Button.CreateSimpleButton("btnL", "←");
+  buttonLEFT.top = "-100px";
+  buttonLEFT.left = "50px";
+  buttonLEFT.width = "45px";
+  buttonLEFT.height = "45px";
+  buttonLEFT.color = "white";
+  buttonLEFT.background = "green";
+  buttonLEFT.horizontalAlignment = GUI.Control.HORIZONTAL_ALIGNMENT_LEFT;
+  buttonLEFT.verticalAlignment = GUI.Control.VERTICAL_ALIGNMENT_BOTTOM;
+  buttonLEFT.onPointerDownObservable.add( () => 
+  {
+    OnDirectionButtonPointerDown( "LEFT" );   
+  });
+  buttonLEFT.onPointerUpObservable.add( () => 
+  {
+    OnDirectionButtonPointerUp( "LEFT" );
+  });
+  var buttonRIGHT = GUI.Button.CreateSimpleButton("btnR", "→");
+  buttonRIGHT.top = "-100px";
+  buttonRIGHT.left = "150px";
+  buttonRIGHT.width = "45px";
+  buttonRIGHT.height = "45px";
+  buttonRIGHT.color = "white";
+  buttonRIGHT.background = "green";
+  buttonRIGHT.horizontalAlignment = GUI.Control.HORIZONTAL_ALIGNMENT_LEFT;
+  buttonRIGHT.verticalAlignment = GUI.Control.VERTICAL_ALIGNMENT_BOTTOM;
+  buttonRIGHT.onPointerDownObservable.add( () => 
+  {
+    OnDirectionButtonPointerDown( "RIGHT" ); 
+  });
+  buttonRIGHT.onPointerUpObservable.add( () => 
+  {
+    OnDirectionButtonPointerUp( "RIGHT" );
+  });
+  var buttonFRONT = GUI.Button.CreateSimpleButton("btnF", "△");
+  buttonFRONT.top = "-125px";
+  buttonFRONT.left = "200px";
+  buttonFRONT.width = "45px";
+  buttonFRONT.height = "45px";
+  buttonFRONT.color = "white";
+  buttonFRONT.background = "green";
+  buttonFRONT.horizontalAlignment = GUI.Control.HORIZONTAL_ALIGNMENT_LEFT;
+  buttonFRONT.verticalAlignment = GUI.Control.VERTICAL_ALIGNMENT_BOTTOM;
+  buttonFRONT.onPointerDownObservable.add( () => 
+  {
+    OnDirectionButtonPointerDown( "FRONT" ); 
+  });
+  buttonFRONT.onPointerUpObservable.add( () => 
+  {
+    OnDirectionButtonPointerUp( "FRONT" );
+  });
+  var buttonBACK = GUI.Button.CreateSimpleButton("btnB", "▽");
+  buttonBACK.top = "-75px";
+  buttonBACK.left = "200px";
+  buttonBACK.width = "45px";
+  buttonBACK.height = "45px";
+  buttonBACK.color = "white";
+  buttonBACK.background = "green";
+  buttonBACK.horizontalAlignment = GUI.Control.HORIZONTAL_ALIGNMENT_LEFT;
+  buttonBACK.verticalAlignment = GUI.Control.VERTICAL_ALIGNMENT_BOTTOM;
+  buttonBACK.onPointerDownObservable.add( () => 
+  {
+    OnDirectionButtonPointerDown( "BACK" ); 
+  });
+  buttonBACK.onPointerUpObservable.add( () => 
+  {
+    OnDirectionButtonPointerUp( "BACK" );
+  });
+
+
+
+
+  var buttonCENTER = GUI.Button.CreateSimpleButton("btnC", "〇");
+  buttonCENTER.top = "-100px";
+  buttonCENTER.left = "100px";
+  buttonCENTER.width = "45px";
+  buttonCENTER.height = "45px";
+  buttonCENTER.color = "white";
+  buttonCENTER.background = "green";
+  buttonCENTER.horizontalAlignment = GUI.Control.HORIZONTAL_ALIGNMENT_LEFT;
+  buttonCENTER.verticalAlignment = GUI.Control.VERTICAL_ALIGNMENT_BOTTOM;
+  buttonCENTER.onPointerDownObservable.add( () => 
+  {
+    console.log( "center click" ); 
+    camera.position = new Vector3( 0, 1, -3 );
+    console.log( camera.position );
+  });
+
+  advancedTextureButton.addControl( buttonTOP );
+  advancedTextureButton.addControl( buttonBOTTOM );
+  advancedTextureButton.addControl( buttonLEFT );
+  advancedTextureButton.addControl( buttonRIGHT );
+  advancedTextureButton.addControl( buttonFRONT );
+  advancedTextureButton.addControl( buttonBACK );
+  advancedTextureButton.addControl( buttonCENTER );
 
   // ScreenUI/Image
   // var img = new GUI.Image( "window", "/assets/window.png" );
@@ -459,6 +600,53 @@ main();
 
 
 // function.
+
+var isMoving : Boolean = false;
+var currentSelectedButton : string = "NONE";
+function AddPosition( key : String, camera : UniversalCamera )
+{
+  var x = 0;
+  var y = 0;
+  var z = 0
+  switch( key )
+  {
+    case "TOP": { x = 0; y = 0.01; break; } 
+    case "BOTTOM": { x = 0; y = -0.01; break; } 
+    case "LEFT": { x = -0.01; y = 0; break; } 
+    case "RIGHT": { x = 0.01; y = 0; break; } 
+    case "FRONT": { z = 0.01; break; } 
+    case "BACK": { z = -0.01; break; } 
+  }
+
+  var current = camera.position;
+  current.x += x;
+  current.y += y;
+  current.z += z;
+  camera.position = current;
+} 
+
+function OnCameraUpdate( camera : UniversalCamera )
+{
+  if( isMoving == true && currentSelectedButton != "NONE" )
+  {
+    AddPosition( currentSelectedButton, camera );
+  }
+}
+
+function OnDirectionButtonPointerDown( key : string )
+{
+  isMoving = true;
+  currentSelectedButton = key;
+
+  console.log( key + "のボタンクリック開始." );
+}
+function OnDirectionButtonPointerUp( key : string )
+{
+  isMoving = false;
+  currentSelectedButton = "NONE";
+
+  console.log( key + "のボタンクリック終了." );
+}
 
 
 
